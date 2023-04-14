094a3b55c89773682097b91e835df25b08e103de
Siddhartha Singh <siddharthasingh@Siddharthas-MacBook-Air.local>
2023-04-15 02:56:04 +0530

    Just changed

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get("/", function(req, res){
 });
 
 
-app.listen (3000, function(){
-console.log("Server is running on port 3000.");
+app.listen (1234, function(){
+console.log("Server is running on port 1234.");
 });
