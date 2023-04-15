b56c7c58e5b4b1f93f8c3f6bce3210497011c00e
Siddhartha Singh <siddharthasingh@Siddharthas-MacBook-Air.local>
2023-04-15 15:17:22 +0530

    2nd commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const https = require('https');
 
 app.get("/", function(req, res){
-  const url="https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=f6c13c65f0e9c219fab9e505f3c01b81"
+  const url="https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=2c147ea67598eb536f843a3312a1753e"
   https.get(url,function(response){
 
     response.on("data",function(data){
